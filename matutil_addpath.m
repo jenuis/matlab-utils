8a57a7f2eaa5ca6e304fe12a432d70e04e4b081e
Xiang Liu <jent.le@hotmail.com>
2024-05-30 15:10:54 +0800

    optimize matutil_addpath making the run faster

--- matutil_addpath.m
+++ matutil_addpath.m
@@ -1,28 +1,18 @@
 function matutil_addpath(module_name)
-%% check arguments
-if nargin < 1
-    module_name = '';
-end
-%% get sub dirs
+%% get current repo path
 curr_file = mfilename('fullpath');
-
 curr_dir = fileparts(curr_file);
-
+%% get sub dirs
 subdirs = dir(curr_dir);
-%% add path
-if ~isempty(module_name)
-    assert(any(strcmpi({subdirs(:).name}, module_name)), ['"matlab-utils. ' module_name '" not exist!'])
+subdirs(~[subdirs.isdir]) = [];
+subdirs = {subdirs.name};
+subdirs(startsWith(subdirs, '.')) = [];
+%% add all sub dirs
+if nargin < 1
+    subdirs = cellfun(@(p) genpath(fullfile(curr_dir, p)), subdirs, 'UniformOutput', false);
+    addpath(subdirs{:})
+    return
 end
-
-for i=1:length(subdirs)
-    subfolder = subdirs(i).name;
-    if subfolder(1) == '.' || ...
-            ~subdirs(i).isdir || ...
-            strcmpi(subfolder, 'license')
-        continue
-    end
-    if ~isempty(module_name) && ~strcmpi(module_name, subfolder)
-        continue
-    end
-    addpath(genpath(fullfile(curr_dir ,subfolder)))
-end
+%% add module
+assert(any(strcmpi(subdirs, module_name)), ['"matlab-utils.' module_name '" not exist!'])
+addpath(genpath(fullfile(curr_dir, module_name)))
